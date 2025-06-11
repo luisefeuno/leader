cb97771907e9bac981d879f8ddfb3329cd0b4456
Luis C. Pérez <luiscarlos@efeuno.es>
2025-06-11 14:09:34 +0200

    Models Transportes - envio Json de datos

--- models/Transportes.php
+++ models/Transportes.php
@@ -1,11 +1,11 @@
 <?php
-      $json_string = json_encode('');
-      $file = 'orden-Tse.json';
-      file_put_contents($file, $json_string);
+$json_string = json_encode('');
+$file = 'orden-Tse.json';
+file_put_contents($file, $json_string);
 class Transporte extends Conectar
-{   
-   
-    
+{
+
+
     public function recogerOrden($idOrden)
     {
         $idOrden = 1;
@@ -13,57 +13,52 @@ class Transporte extends Conectar
         parent::set_names();
         $sql = "SELECT * FROM `orden-Transporte` WHERE id_orden = $idOrden";
 
-     
+
         $sql = $conectar->prepare($sql);
         $sql->execute();
         return $resultado = $sql->fetchAll();
-
     }
     public function recogerOrdenToken($idOrdenToken)
     {
-       
+
         $conectar = parent::conexion();
         parent::set_names();
         $sql = "SELECT * FROM `orden-viaje-view` WHERE tokenOrden = '$idOrdenToken'";
-      
+
         $sql = $conectar->prepare($sql);
         $sql->execute();
         return $resultado = $sql->fetch();
-
     }
-    public function recogerOrdenTokenAll($idOrdenToken,$idviaje)
+    public function recogerOrdenTokenAll($idOrdenToken, $idviaje)
     {
-       
+
         $conectar = parent::conexion();
         parent::set_names();
-        if(empty($idviaje)){
+        if (empty($idviaje)) {
             $sql = "SELECT * FROM `orden-viaje-view` WHERE tokenOrden = '$idOrdenToken'";
-
         } else {
             $sql = "SELECT * FROM `orden-viaje-view` WHERE tokenOrden = '$idOrdenToken' AND `idViaje`=$idviaje";
-
         }
 
         $sql = $conectar->prepare($sql);
         $sql->execute();
         return $resultado = $sql->fetchAll();
-
     }
-    
+
 
     public function mostrarOrdenes()
     {
         $conectar = parent::conexion();
         parent::set_names();
         $sql = "SELECT * FROM `orden-Transporte` ORDER BY `TTE_COD` DESC";
-      $json_string = json_encode($sql);
-      $file = 'orden-Transporte.json';
-      file_put_contents($file, $json_string);
+        $json_string = json_encode($sql);
+        $file = 'orden-Transporte.json';
+        file_put_contents($file, $json_string);
         $sql = $conectar->prepare($sql);
         $sql->execute();
         return $resultado = $sql->fetchAll();
     }
-    
+
     public function mostrarOrdenesXIdTrabajador($idTrabajador)
     {
         $conectar = parent::conexion();
@@ -73,19 +68,19 @@ class Transporte extends Conectar
         $sql->execute();
         return $resultado = $sql->fetchAll();
     }
-    
+
     public function recogerIncidencia($idIncidencia)
     {
         $conectar = parent::conexion();
         parent::set_names();
         $sql = "SELECT * FROM `view_OrdenXIncidenciaXSituaciones` WHERE idIncidencia = $idIncidencia";
-      
+
         $sql = $conectar->prepare($sql);
         $sql->execute();
         return $resultado = $sql->fetchAll();
     }
-    
-   public function mostrarIncidenciaXOrden($idOrden)
+
+    public function mostrarIncidenciaXOrden($idOrden)
     {
         $conectar = parent::conexion();
         parent::set_names();
@@ -95,10 +90,10 @@ class Transporte extends Conectar
 
         $sql = $conectar->prepare($sql);
         $sql->execute($resultado = $sql->fetchAll());
-       
+
         return $resultado = $sql->fetchAll();
     }
-    
+
     public function listarSituacion()
     {
         $conectar = parent::conexion();
@@ -109,11 +104,11 @@ class Transporte extends Conectar
         return $resultado = $sql->fetchAll();
     }
 
-    public function guardarIncidencia($idOrden,$reportante,$selectSituacion,$selectViaje,$fechaIncidencia,$descripcionIncidencia,$documentos)
+    public function guardarIncidencia($idOrden, $reportante, $selectSituacion, $selectViaje, $fechaIncidencia, $descripcionIncidencia, $documentos)
     {
         $conectar = parent::conexion();
         parent::set_names();
-        $documentosJson = json_encode($documentos); 
+        $documentosJson = json_encode($documentos);
 
         $idOrdenSinBarra = str_replace("/", "", $idOrden);
         // Establecer la zona horaria de Madrid
@@ -123,12 +118,55 @@ class Transporte extends Conectar
         $sql = "INSERT INTO `incidencia-Transporte`( `idOrden_Incidencia`, `transportistaIncidencia`, `situacionIncidencia`,`viajeIncidencia`, `documento`, `descripcion`, `fechaCreacionIncidencia`, `estIncidencia`) 
         VALUES ('$idOrdenSinBarra','$reportante',$selectSituacion,'$selectViaje','$documentosJson','$descripcionIncidencia','$fechaIncidencia',1)";
 
+        // Determine situacionLiteral based on $selectSituacion
+        switch ($selectSituacion) {
+            case 1:
+                $situacionLiteral = "Sin situacion";
+                break;
+            case 2:
+                $situacionLiteral = "En reparto";
+                break;
+            case 3:
+                $situacionLiteral = "En entrega";
+                break;
+            default:
+                $situacionLiteral = "Situacion desconocida";
+                break;
+        }
+
+
+        // Create a JSON object with the incidence data
+        $incidenceData = [
+            'idOrden' => $idOrdenSinBarra,
+            'reportante' => $reportante,
+            'situacion' => $selectSituacion,
+            'situacionLiteral' => $situacionLiteral,
+            'viaje' => $selectViaje,
+            'fechaIncidencia' => $fechaIncidencia,
+            'descripcion' => $descripcionIncidencia,
+            'documentos' => $documentos
+        ];
+
+        // Convert the data to JSON format
+        $jsonString = json_encode($incidenceData, JSON_PRETTY_PRINT);
+
+        // Define the file path and name
+        $filePath = "../view/Ordenes/envios/I_{$idOrdenSinBarra}.json";
+
+        // Save the JSON to the specified file
+        file_put_contents($filePath, $jsonString);
+
+// Vamos a guardar la imagen en la carpeta de envios
+
+
+
         $sql = $conectar->prepare($sql);
         $sql->execute();
         return $resultado = $sql->fetchAll();
     }
-    
-    public function cambiarEstado($idTransporte){
+
+    public function cambiarEstado($idTransporte)
+    {
 
         $conectar = parent::conexion();
         parent::set_names();
@@ -138,20 +176,20 @@ class Transporte extends Conectar
         return $resultado = $sql->fetchAll();
     }
 
-        
-    public function guardarContenedor($idOrden,$numeroContenedor){
 
+    public function guardarContenedor($idOrden, $numeroContenedor)
+    {
         try {
             $conectar = parent::conexion();
             parent::set_names();
-            
+
             $sql = "UPDATE `orden-Transporte` SET `contenedorActivo`= :numeroContenedor WHERE num_transporte = :idOrden";
             $stmt = $conectar->prepare($sql);
-            
+
             // Bind parameters to prevent SQL injection
             $stmt->bindParam(':numeroContenedor', $numeroContenedor, PDO::PARAM_STR);
             $stmt->bindParam(':idOrden', $idOrden, PDO::PARAM_STR);
-            
+
             // Execute the statement
             if ($stmt->execute()) {
                 return 1;
@@ -164,19 +202,25 @@ class Transporte extends Conectar
             return 0;
         }
     }
-    public function guardarPrecinto($idOrden,$precinto){
+
+
+
+
+
+    public function guardarPrecinto($idOrden, $precinto)
+    {
 
         try {
             $conectar = parent::conexion();
             parent::set_names();
-            
+
             $sql = "UPDATE `orden-Transporte` SET `precintoActivo`= :precinto WHERE num_transporte = :idOrden";
             $stmt = $conectar->prepare($sql);
-            
+
             // Bind parameters to prevent SQL injection
             $stmt->bindParam(':precinto', $precinto, PDO::PARAM_STR);
             $stmt->bindParam(':idOrden', $idOrden, PDO::PARAM_STR);
-            
+
             // Execute the statement
             if ($stmt->execute()) {
                 return 1;
@@ -190,7 +234,8 @@ class Transporte extends Conectar
         }
     }
 
-    public function recogerViajesxOrden($idOrden){
+    public function recogerViajesxOrden($idOrden)
+    {
         $conectar = parent::conexion();
         parent::set_names();
         $sql = "SELECT * FROM `viaje-Transporte` WHERE id_OrdenViajeTransporte = $idOrden;";
@@ -198,86 +243,76 @@ class Transporte extends Conectar
         $sql = $conectar->prepare($sql);
         $sql->execute();
         return $resultado = $sql->fetchAll();
-
     }
-    
-    
-    public function recogerViajesxID($idViaje){
+
+
+    public function recogerViajesxID($idViaje)
+    {
         $conectar = parent::conexion();
         parent::set_names();
         $sql = "SELECT * FROM `viaje-Transporte` WHERE idViaje = $idViaje;";
-        
+
         $sql = $conectar->prepare($sql);
         $sql->execute();
         return $resultado = $sql->fetchAll();
-
     }
-    
-    public function recogerViajesxIDOrden($idViaje){
+
+    public function recogerViajesxIDOrden($idViaje)
+    {
         $conectar = parent::conexion();
         parent::set_names();
         $sql = "SELECT * FROM `orden-viaje-view` WHERE idViaje = $idViaje;";
-        
+
         $sql = $conectar->prepare($sql);
         $sql->execute();
         return $resultado = $sql->fetchAll();
-
     }
 
-    public function actualizarViaje($idViaje,$fechaLlegada,$fechaSalida,$observacionesViaje){
+    public function actualizarViaje($idViaje, $fechaLlegada, $fechaSalida, $observacionesViaje)
+    {
         $conectar = parent::conexion();
         parent::set_names();
         $sql = "UPDATE `viaje-Transporte` SET 
         `fechaLlegadaViaje`='$fechaLlegada',`fechaSalidaViaje`='$fechaSalida',`ObservacionViaje`='$observacionesViaje',`FirmaViajeReceptor`='' WHERE idViaje = $idViaje";
- 
+
         $sql = $conectar->prepare($sql);
         $sql->execute();
         return $resultado = $sql->fetchAll();
-
     }
-    public function recogerFirmaPerfil(){
+    public function recogerFirmaPerfil()
+    {
         $dniUsu = $_SESSION["usu_identificacionFiscal"];
         $conectar = parent::conexion();
         parent::set_names();
         $sql = "SELECT * FROM `transportistas-Transporte` WHERE `idTransportistaLeader` = '$dniUsu'";
- 
+
         $sql = $conectar->prepare($sql);
         $sql->execute();
         return $resultado = $sql->fetchAll();
-
     }
 
-    public function actualizarFirma($idViaje,$signature,$nombreInput,$correoInput,$DNIinput,$autorFirma,$idOrdenReceptor){
+    public function actualizarFirma($idViaje, $signature, $nombreInput, $correoInput, $DNIinput, $autorFirma, $idOrdenReceptor)
+    {
         $conectar = parent::conexion();
         parent::set_names();
 
-        if($autorFirma == 'conductor'){
+        if ($autorFirma == 'conductor') {
             $sql = "UPDATE `viaje-Transporte` SET 
             `FirmaViajeConductor`='$signature',`nombreViajeConductor`='$nombreInput',`dniViajeConductor`='$DNIinput' 
             WHERE idViaje = $idViaje";
-           
-        }else if($autorFirma == 'receptor'){ 
+        } else if ($autorFirma == 'receptor') {
             $sql = "UPDATE `viaje-Transporte` SET 
             `FirmaViajeReceptor`='$signature',`nombreViajeReceptor`='$nombreInput',`correoViajeReceptor`='$correoInput',`dniViajeReceptor`='$DNIinput' 
             WHERE idViaje = $idViaje";
-           
-        }else if($autorFirma == 'cliente'){
+        } else if ($autorFirma == 'cliente') {
             $sql = "UPDATE `orden-Transporte` SET 
             `nombreCliente`='$nombreInput',`correoCliente`='$correoInput',`dniCliente`='$DNIinput',`firmaCliente`='$signature'
              WHERE `tokenOrden` = '$idOrdenReceptor'";
-        
-
         }
-        
-   
+
+
         $sql = $conectar->prepare($sql);
         $sql->execute();
         return $resultado = $sql->fetchAll();
-
     }
-    
-    
-
-
-
 }
